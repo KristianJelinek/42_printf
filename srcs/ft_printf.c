fbfe4c25408945bc2ed56b91793a7cd9b64d98e4
KristianJelinek <117808001+KristianJelinek@users.noreply.github.com>
2023-12-11 16:46:01 +0100

    Update ft_printf.c

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -12,51 +12,61 @@
 
 #include "../includes/ft_printf.h"
 
-static	void	ft_printf_check(char s, va_list *args, int *len, int *i)
+static void ft_printf_check(char s, va_list *args, int *len, int *i)
 {
-	if (s == 's')
-		ft_string(va_arg(*args, char *), len);
-	else if (s == 'd' || s == 'i')
-		ft_number(va_arg(*args, int), len);
-	else if (s == 'u')
-		ft_unsigned_int(va_arg(*args, unsigned int), len);
-	else if (s == 'x')
-		ft_hexadecimal(va_arg(*args, unsigned int), len, 'x');
-	else if (s == 'X')
-		ft_hexadecimal(va_arg(*args, unsigned int), len, 'X');
-	else if (s == 'p')
-		ft_pointer(va_arg(*args, size_t), len);
-	else if (s == 'c')
-		ft_putcharacter_len(va_arg(*args, int), len);
-	else if (s == '%')
-		ft_putcharacter_len('%', len);
-	else
-		(*i)--;
+    // Rozpoznání formátovacího specifikátoru a volání odpovídající funkce pro zpracování
+    if (s == 's')
+        ft_string(va_arg(*args, char *), len);
+    else if (s == 'd' || s == 'i')
+        ft_number(va_arg(*args, int), len);
+    else if (s == 'u')
+        ft_unsigned_int(va_arg(*args, unsigned int), len);
+    else if (s == 'x')
+        ft_hexadecimal(va_arg(*args, unsigned int), len, 'x');
+    else if (s == 'X')
+        ft_hexadecimal(va_arg(*args, unsigned int), len, 'X');
+    else if (s == 'p')
+        ft_pointer(va_arg(*args, size_t), len);
+    else if (s == 'c')
+        ft_putcharacter_len(va_arg(*args, int), len);
+    else if (s == '%')
+        ft_putcharacter_len('%', len);
+    else
+        (*i)--;
 }
 
-int	ft_printf(const char *string, ...)
+int ft_printf(const char *string, ...)
 {
-	va_list	args;
-	int		i;
-	int		len;
+    va_list args;
+    int i;
+    int len;
 
-	i = 0;
-	len = 0;
-	va_start(args, string);
-	while (string[i] != '\0')
-	{
-		if (string[i] == '%')
-		{
-			i++;
-			ft_printf_check(string[i], &args, &len, &i);
-			i++;
-		}
-		else
-		{
-			ft_putcharacter_len((char)string[i], &len);
-			i++;
-		}
-	}
-	va_end(args);
-	return (len);
+    // Inicializace indexu i a délky výstupu
+    i = 0;
+    len = 0;
+
+    // Inicializace proměnné argumentů
+    va_start(args, string);
+
+    // Zpracování řetězce formátu
+    while (string[i] != '\0')
+    {
+        if (string[i] == '%')
+        {
+            i++; // Přeskočení znaku '%'
+            ft_printf_check(string[i], &args, &len, &i); // Zpracování formátovacího specifikátoru
+            i++; // Přeskočení zpracovaného specifikátoru
+        }
+        else
+        {
+            ft_putcharacter_len((char)string[i], &len); // Výpis běžného znaku
+            i++; // Přeskočení běžného znaku
+        }
+    }
+
+    // Ukončení práce s proměnnými argumenty
+    va_end(args);
+
+    // Vrácení celkové délky vytvořeného textu
+    return (len);
 }
