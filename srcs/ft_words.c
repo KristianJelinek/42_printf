6904290cefee09245522196ba9adbac49cdb429b
KristianJelinek <117808001+KristianJelinek@users.noreply.github.com>
2023-12-11 16:48:21 +0100

    Update ft_words.c

--- srcs/ft_words.c
+++ srcs/ft_words.c
@@ -12,25 +12,35 @@
 
 #include "../includes/ft_printf.h"
 
-void	ft_putcharacter_len(char character, int *len)
+void ft_putcharacter_len(char c, int *len)
 {
-	write(1, &character, 1);
-	(*len)++;
+    // Výpis jednoho znaku na standardní výstup
+    write(1, &c, 1);
+    
+    // Aktualizace délky vytvořeného textu
+    (*len)++;
 }
-void	ft_string(char *args, int *len)
+
+void ft_string(char *args, int *len)
 {
-	size_t	i;
-	
-	i = 0;
-	if (!args)
-	{
-		write(1, "(null)", 6);
-		(*len) += 6;
-		return ;
-	}
-	while (args[i] != '\0')
-	{
-		ft_putcharacter_len(args[i], len);
-		i++;
-	}
+    size_t i;
+
+    // Inicializace indexu pro postupné procházení řetězce
+    i = 0;
+
+    // Zvláštní případ pro NULL řetězec
+    if (!args)
+    {
+        write(1, "(null)", 6);
+        (*len) += 6;
+        return;
+    }
+
+    // Postupné procházení řetězce
+    while (args[i] != '\0')
+    {
+        // Volání funkce pro výpis jednoho znaku a aktualizace délky
+        ft_putcharacter_len(args[i], len);
+        i++;
+    }
 }
