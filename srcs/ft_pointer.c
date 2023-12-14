6ea9057f527f3596a6313778b84d9c4690e0a0ae
KristianJelinek <117808001+KristianJelinek@users.noreply.github.com>
2023-12-14 17:31:17 +0100

    Update ft_pointer.c

--- srcs/ft_pointer.c
+++ srcs/ft_pointer.c
@@ -5,67 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kjelinek < kjelinek@student.42prague.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/08 09:57:59 by kjelinek          #+#    #+#             */
-/*   Updated: 2023/12/14 11:08:58 by kjelinek         ###   ########.fr       */
+/*   Created: 2023/12/10 14:33:48 by kjelinek          #+#    #+#             */
+/*   Updated: 2023/12/14 15:56:16 by kjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void	ft_pointer2(size_t ptr, int *len)
-{
-	char str[25] = {0};  // Inicializace pole na nulové hodnoty
-	int i = 0;
-	char *base_char = "0123456789abcdef";
-	// Speciální případ pro nulu
-	if (ptr == 0)
-	{
-		ft_putcharacter_len('0', len);
-		return ;
-	}
-	// Převod na hexadecimální reprezentaci
-	while (ptr != 0 && i < 24)
-	{
-		str[i] = base_char[ptr % 16];
-		ptr /= 16;
-		i++;
-	}
-	// Výpis hexadecimální reprezentace
-	while (i--)
-		ft_putcharacter_len(str[i], len);
-}
+static char	g_hex[] = "0123456789abcdef";
 
-void	ft_pointer(size_t ptr, int *len)
+void	ft_putpointer(size_t ptr, int *len)
 {
-	char str[25] = {0};  // Inicializace pole na nulové hodnoty
-	int i = 0;
-	char *base_char = "0123456789abcdef";
-	// Speciální případ pro nulový ukazatel
+	char	str[25];
+	int		i;
+
+	i = 0;
 	if (!ptr)
 	{
 		write(1, "(nil)", 5);
 		*len += 5;
 		return ;
 	}
-	// Převod na hexadecimální reprezentaci
-	while (ptr != 0 && i < 24)
+	while (ptr != 0)
 	{
-		str[i++] = base_char[ptr % 16];
+		str[i++] = g_hex[ptr % 16];
 		ptr /= 16;
 	}
-	// Výpis hexadecimální reprezentace
+	write(1, "0x", 2);
+	*len += 2;
 	while (i--)
-	{
 		ft_putcharacter_len(str[i], len);
-	}
 }
 
-int main() {
-    size_t test_ptr = 12345; // Změňte na libovolnou testovací hodnotu
-    int length = 0;
-
-    ft_pointer(test_ptr, &length);
-    printf("\nDélka výstupu: %d\n", length);
-
-    return 0;
+void	ft_pointer(size_t ptr, int *len)
+{
+	ft_putpointer(ptr, len);
 }
