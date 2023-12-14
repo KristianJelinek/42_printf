8cb4e20694f736563cd131dea2fe4f397dbcd4af
KristianJelinek <117808001+KristianJelinek@users.noreply.github.com>
2023-12-14 17:32:47 +0100

    Update ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kjelinek < kjelinek@student.42prague.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 11:40:18 by k                 #+#    #+#             */
-/*   Updated: 2023/12/14 11:10:05 by kjelinek         ###   ########.fr       */
+/*   Created: 2023/12/10 14:33:08 by kjelinek          #+#    #+#             */
+/*   Updated: 2023/12/14 17:27:43 by kjelinek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@
 # include <stdlib.h>
 # include <stddef.h>
 
-
-void 	ft_putcharacter_len(char c, int *len);
+void	ft_putcharacter_len(char character, int *len);
 void	ft_string(char *args, int *len);
 void	ft_number(int number, int *len);
 void	ft_hexadecimal(unsigned int x, int *len, char x_or_x);
 void	ft_unsigned_int(unsigned int u, int *len);
-void	ft_pointer2(size_t ptr, int *len);
 void	ft_pointer(size_t pointer, int *len);
 int		ft_printf(const char *string, ...);
+void	ft_putcharacter_len(char c, int *len);
 
 #endif
